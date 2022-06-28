b43b317710d55e965b61fb7cc1b982fed3958e11
ShadabShariff <shadab2shariff@gmail.com>
2022-06-28 16:08:31 +0530

    Add files via upload

---
+++ case_controller/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name='case_controller',
+    version='0.0.1',
+    author='test',
+    author_email='test@test.com',
+    description='Testing installation of Package',
+    long_description="Read from file for longer description",
+    long_description_content_type="text/markdown",
+    url='',
+    project_urls = {
+        "Bug Tracker": "/issues"
+    },
+    packages=['case_controller'],
+)

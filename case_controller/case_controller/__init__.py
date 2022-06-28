3c4313a9b4c8f75b12cf71802f50c77bb04a53a2
ShadabShariff <shadab2shariff@gmail.com>
2022-06-28 16:14:57 +0530

    Delete case_controller directory

--- case_controller/case_controller/__init__.py
+++
@@ -1,4 +0,0 @@
-# This is so that you can import case_controller or import lower from case_controller
-# in stead of from case_controller.functions import lower
-
-from .functions import lower, upper
--- case_controller/case_controller/functions.py
+++
@@ -1,5 +0,0 @@
-def lower(_i):
-    return str(_i).lower()
-
-def upper(_i):
-    return str(_i).upper()
